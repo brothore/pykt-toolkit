4c6c6d990b713b03f10ceee358e5a41c3ee9a15a
brothore <brothore@outlook.com>
2026-04-13 18:20:19 +0800

    ukt train

--- examples/wandb_ukt_train.py
+++ examples/wandb_ukt_train.py
@@ -9,6 +9,7 @@ if __name__ == "__main__":
     parser.add_argument("--save_dir", type=str, default="saved_model")
     parser.add_argument("--seed", type=int, default=3407)
     parser.add_argument("--fold", type=int, default=0)
+    parser.add_argument("--batch_size", type=int, default=16)
     parser.add_argument("--dropout", type=float, default=0.2)
     
     parser.add_argument("--d_model", type=int, default=256)
